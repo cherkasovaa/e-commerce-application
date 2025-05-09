b512c4160f7d1164aa03c8d73ef5707d07d2726c
cherkasovaa <alina.ch1202@gmail.com>
2025-05-09 14:07:14 +0300

    feat: implement a 404 page
    - Add `dotlottie-react` plugin
    - Update Vite config for Lottie support
    - Add clear message for the user
    - Add Lottie animation
    - Add back to home page button

--- src/pages/not-found/ui/NotFoundPage.tsx
+++ src/pages/not-found/ui/NotFoundPage.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import { Typography, Box, Stack } from '@mui/material';
+
+import pageNotFoundAnimation from '@/pages/assets/404_animation.lottie';
+import {
+  BACK_BUTTON_TEXT,
+  PAGE_NOT_FOUNT_DESCRIPTION,
+  PAGE_NOT_FOUNT_TITLE,
+} from '../constants/constants';
+import { NavigationButton } from '@/shared/ui/NavigationButton/NavigationButton';
 
 export const NotFoundPage: React.FC = () => (
   <>
-    <h1>NotFoundPage</h1>
+    <Stack
+      direction={{ xs: 'column-reverse', md: 'row' }}
+      spacing={10}
+      alignItems="center"
+      justifyContent="center"
+      style={{ height: '100%' }}
+    >
+      <DotLottieReact
+        src={pageNotFoundAnimation}
+        loop
+        autoplay
+        style={{ width: 300, height: 'auto' }}
+      />
+
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          textAlign: { md: 'left', xs: 'center' },
+        }}
+      >
+        <Typography variant="h1">{PAGE_NOT_FOUNT_TITLE}</Typography>
+        <Typography component="p">{PAGE_NOT_FOUNT_DESCRIPTION}</Typography>
+
+        <NavigationButton
+          to="/"
+          sx={{
+            maxWidth: 'max-content',
+            marginTop: 3,
+            alignSelf: { md: 'flex-start', xs: 'center' },
+          }}
+        >
+          {BACK_BUTTON_TEXT}
+        </NavigationButton>
+      </Box>
+    </Stack>
   </>
 );
