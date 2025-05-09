6e1e3870f5bf1e38b2f2eab5674accc46d1f6de5
cherkasovaa <alina.ch1202@gmail.com>
2025-05-09 10:46:03 +0300

    feat: routing implementation
    - Implement routing for navigation between login, registration, and main pages.
    - Add page stubs for main, login, register, and not-found pages.

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginPage } from '@/pages/login';
+import { MainPage } from '@/pages/main';
+import { RegisterPage } from '@/pages/register';
+import { NotFoundPage } from '@/pages/not-found';
 
 export const Router = (): React.JSX.Element => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
