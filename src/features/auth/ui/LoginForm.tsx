a11a1d84cbe186ba3b635f964f6aa4f02b6111c1
Akseenja <klasik1995@bk.ru>
2025-05-10 14:47:29 +0300

    feat: add base layout for login page

--- src/features/auth/ui/LoginForm.tsx
+++ src/features/auth/ui/LoginForm.tsx
@@ -1,8 +1,59 @@
-import { useEffect } from 'react';
+import { Box, Button, TextField } from '@mui/material';
+import { type FormEvent, useState, type JSX } from 'react';
 
-export const LoginForm = () => {
-  useEffect(() => {
-    console.log('form initialized');
-  }, []);
-  return <div>LoginForm</div>;
+export const LoginForm = (): JSX.Element => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setIsLoading(true);
+    console.log(email, password);
+  };
+
+  const passwordError = false;
+  const emailError = false;
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        mx: 'auto',
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 3,
+        mb: 4,
+      }}
+    >
+      <TextField
+        id="outlined-basic"
+        label="Enter login"
+        variant="outlined"
+        fullWidth
+        autoFocus
+        helperText={emailError}
+        onChange={(e) => setEmail(e.target.value)}
+        color="primary"
+        focused
+      ></TextField>
+      <TextField
+        id="outlined-basic"
+        label="Enter password"
+        variant="outlined"
+        fullWidth
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        helperText={passwordError}
+        color="primary"
+        focused
+      ></TextField>
+      <Button type="submit" variant="contained" fullWidth loading={loading}>
+        Submit
+      </Button>
+    </Box>
+  );
 };
--- src/widgets/Login/ui/Login.tsx
+++ src/widgets/Login/ui/Login.tsx
@@ -1,14 +1,40 @@
 import { LoginForm } from '@/features/auth';
-import { Typography } from '@mui/material';
+import { Avatar, Paper, Typography } from '@mui/material';
+import LockOutlineIcon from '@mui/icons-material/LockOutline';
+import { type JSX } from 'react';
 
-export const Login = () => {
+export const Login = (): JSX.Element => {
   return (
-    <div>
-      <Typography variant="h2" mb={2}>
-        Войдите в профиль!
+    <Paper
+      elevation={10}
+      sx={{
+        marginTop: 8,
+        padding: 2,
+      }}
+    >
+      <Avatar
+        sx={{
+          mx: 'auto',
+          bgcolor: 'secondary.contrastText',
+          textAlign: 'center',
+          mb: 1,
+        }}
+      >
+        <LockOutlineIcon />
+      </Avatar>
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{
+          mx: 'auto',
+          textAlign: 'center',
+          mb: 4,
+        }}
+      >
+        Sign in
       </Typography>
       <LoginForm></LoginForm>
-      <div>registration link</div>
-    </div>
+      <div>New user?</div>
+    </Paper>
   );
 };
