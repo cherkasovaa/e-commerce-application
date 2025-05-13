c66a0513dd6f43ff085a680af9c77514f01061b9
cherkasovaa <alina.ch1202@gmail.com>
2025-05-13 15:34:50 +0300

    refactor: update routing structure with nested routes

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,15 +1,16 @@
 import { Header } from '@/widgets/Header';
 import { Box, Container } from '@mui/material';
 import React from 'react';
-import type { LayoutProps } from './types/interfaces';
+import { Outlet } from 'react-router-dom';
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => (
+export const Layout: React.FC = () => (
   <Container
     style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
   >
     <Header />
+
     <Box component="main" style={{ flex: 1 }}>
-      {children}
+      <Outlet />
     </Box>
   </Container>
 );
