e98328577b2d3b2428425344193a2f4cb383f159
cherkasovaa <alina.ch1202@gmail.com>
2025-05-15 18:37:25 +0300

    feat: refactor and improve header component
    - Create separate Desktop and Mobile menus
    - Show customized menu depending on screen width
    - Render links dynamically

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,16 +1,39 @@
+import { useEffect, useState, type FC } from 'react';
+
 import { AuthButtons, LogoutButton } from '@/features/auth';
 import { Logo } from '@/shared/ui';
-import { AppBar, Toolbar } from '@mui/material';
-import React from 'react';
+import { AppBar, Toolbar, useMediaQuery, useTheme } from '@mui/material';
+import { MenuDesktop } from '@/shared/ui/MenuDesktop/MenuDesktop';
+import { MenuMobile } from '@/shared/ui/MenuMobile/MenuMobile';
+import { APP_ROUTES } from '@/shared/config/routes/routes';
 
-export const Header: React.FC = () => {
-  // localStorage.setItem('isAuthenticated', 'true');
-  const isAuth = localStorage.getItem('isAuthenticated') === 'true';
+export const Header: FC = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    const auth = localStorage.getItem('isAuthenticated') === 'true';
+    setIsAuth(auth);
+  }, []);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const pages = APP_ROUTES.filter((route) => route.meta.showInNavigateMenu);
 
   return (
     <AppBar position="static" color="transparent">
       <Toolbar sx={{ justifyContent: 'space-between' }}>
-        <Logo />
+        {isMobile ? (
+          <>
+            <MenuMobile pages={pages} />
+            <Logo />
+          </>
+        ) : (
+          <>
+            <Logo />
+            <MenuDesktop pages={pages} />
+          </>
+        )}
 
         {isAuth ? <LogoutButton /> : <AuthButtons />}
       </Toolbar>
