2afb53e1134a722a5e68452001d63d2283316380
cherkasovaa <alina.ch1202@gmail.com>
2025-05-15 18:27:55 +0300

    refactor: replace hardcoded paths and names with dynamic imports in AuthButtons.tsx

--- src/features/auth/ui/AuthButtons/AuthButtons.tsx
+++ src/features/auth/ui/AuthButtons/AuthButtons.tsx
@@ -1,14 +1,17 @@
+import type { FC } from 'react';
+
+import { APP_PAGE_NAMES } from '@/shared/config/routes/pageNames';
+import { APP_PATHS } from '@/shared/config/routes/paths';
 import { NavigationButton } from '@/shared/ui';
 import { Box } from '@mui/material';
-import React from 'react';
 
-export const AuthButtons: React.FC = () => (
+export const AuthButtons: FC = () => (
   <Box sx={{ display: 'flex', gap: { xs: 1, sm: 2 } }}>
-    <NavigationButton to="/login" color="inherit">
-      Login
+    <NavigationButton to={APP_PATHS.LOGIN} color="inherit">
+      {APP_PAGE_NAMES.LOGIN}
     </NavigationButton>
-    <NavigationButton to="/register" color="inherit">
-      Register
+    <NavigationButton to={APP_PATHS.REGISTER} color="inherit">
+      {APP_PAGE_NAMES.REGISTER}
     </NavigationButton>
   </Box>
 );
