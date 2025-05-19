10707856499d092e068017979be4daca3f52ccc6
cherkasovaa <alina.ch1202@gmail.com>
2025-05-19 15:47:47 +0300

    feat: add empty footer component as skeleton

--- src/app/layout/Layout.tsx
+++ src/app/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from '@/widgets/Footer';
 import { Header } from '@/widgets/Header';
 import { Box, Container } from '@mui/material';
 import React from 'react';
@@ -5,12 +6,19 @@ import { Outlet } from 'react-router-dom';
 
 export const Layout: React.FC = () => (
   <Container
-    style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      gap: 10,
+    }}
   >
     <Header />
 
     <Box component="main" style={{ flex: 1 }}>
       <Outlet />
     </Box>
+
+    <Footer />
   </Container>
 );
