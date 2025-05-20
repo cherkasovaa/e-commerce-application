5cbadc86d475e20ac6af4f314d79a19760627a9d
cherkasovaa <alina.ch1202@gmail.com>
2025-05-20 15:06:07 +0300

    feat: add footer component

---
+++ src/shared/config/developers.ts
@@ -0,0 +1,16 @@
+import type { AppDeveloper } from '../types/appDevelopers';
+
+export const DEVELOPERS: AppDeveloper[] = [
+  {
+    github: 'https://github.com/akseee',
+    name: 'akseee',
+  },
+  {
+    github: 'https://github.com/aissatsana',
+    name: 'aissatsana',
+  },
+  {
+    github: 'https://github.com/cherkasovaa/',
+    name: 'cherkasovaa',
+  },
+] as const;
