4ee73669c7ce09498270ed063b1e9fb04af1e0db
cherkasovaa <alina.ch1202@gmail.com>
2025-06-01 09:28:20 +0300

    refactor: change null to undefined in hook useProductMeta

--- src/shared/lib/hooks/useProductMeta.ts
+++ src/shared/lib/hooks/useProductMeta.ts
@@ -4,10 +4,10 @@ import type { ProductProjection } from '@commercetools/platform-sdk';
 import { useMemo } from 'react';
 
 export const useProductMeta = (
-  product: ProductProjection | null
-): PageMetaProps | null => {
+  product: ProductProjection | undefined
+): PageMetaProps | undefined => {
   return useMemo(() => {
-    if (!product) return null;
+    if (!product) return undefined;
 
     const title = `${product.name?.[LANGUAGE.EN] || 'Page'} | ${APP_NAME}`;
     const description =
