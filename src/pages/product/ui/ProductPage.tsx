97895a16178338ea2b6d893bf0edeecaef881732
cherkasovaa <alina.ch1202@gmail.com>
2025-06-01 09:26:23 +0300

    refactor: use tanstack query for product data fetching

--- src/pages/product/ui/ProductPage.tsx
+++ src/pages/product/ui/ProductPage.tsx
@@ -1,57 +1,24 @@
 import { PageMeta } from '@/features/ProductMeta';
 import { NotFoundPage } from '@/pages/not-found';
-import { getCategoriesByIds } from '@/shared/api/commerceTools/getCategoriesByIds';
-import { getProductById } from '@/shared/api/commerceTools/getProductById';
 import { useProductMeta } from '@/shared/lib/hooks/useProductMeta';
 import { getDataForProductDetails } from '@/shared/lib/products/getDataForProductDetails';
 import { AdditionalInfoScreen } from '@/widgets/AdditionalInfoScreen';
 import { MediaScreen } from '@/widgets/MediaScreen';
 import { ProductDetails } from '@/widgets/ProductDetails';
-import type { Category, ProductProjection } from '@commercetools/platform-sdk';
 import { Grid } from '@mui/material';
-import { useEffect, useState, type FC } from 'react';
+import { type FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProductSkeleton } from './ProductSkeleton';
+import { useProduct } from '@/shared/lib/hooks/useProduct';
+import { useCategory } from '@/shared/lib/hooks/useCategory';
 
 export const ProductPage: FC = () => {
   const { id } = useParams();
 
-  const [product, setProduct] = useState<ProductProjection | null>(null);
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const { data: product, isLoading: loading, error } = useProduct(id || '');
+  const { data: categories } = useCategory(product?.categories || []);
 
-  useEffect(() => {
-    if (!id) return;
-    setLoading(true);
-
-    async function fetchData(id: string): Promise<void> {
-      try {
-        const productData = await getProductById(id);
-        setProduct(productData);
-
-        if (productData?.categories.length) {
-          const categoryIds = productData.categories.map(
-            (category) => category.id
-          );
-
-          if (categoryIds) {
-            const categoriesData = await getCategoriesByIds(categoryIds);
-            setCategories(categoriesData);
-          }
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchData(id);
-  }, [id]);
-
+  console.log(product);
   const metaData = useProductMeta(product);
 
   if (loading) return <ProductSkeleton />;
@@ -75,7 +42,7 @@ export const ProductPage: FC = () => {
 
         <AdditionalInfoScreen
           key={key}
-          categories={categories}
+          categories={categories ? categories : []}
           gameData={rest}
         />
       </Grid>
---
+++ src/shared/lib/hooks/useProduct.ts
@@ -0,0 +1,13 @@
+import { getProductById } from '@/shared/api/commerceTools/getProductById';
+import type { QueryResult } from '@/shared/types/query';
+import type { ProductProjection } from '@commercetools/platform-sdk';
+import { useQuery } from '@tanstack/react-query';
+
+export function useProduct(id: string): QueryResult<ProductProjection> {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['product', id],
+    queryFn: () => getProductById(id),
+  });
+
+  return { data, isLoading, error };
+}
