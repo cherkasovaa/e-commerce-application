3ce14a3b07fdbb2c5c04b1f6cc2dc942911eb4d6
cherkasovaa <alina.ch1202@gmail.com>
2025-06-06 20:30:48 +0300

    fix: add route to the about page, rss logo and text about the project

--- src/features/AboutIntro/model/constants.ts
+++ src/features/AboutIntro/model/constants.ts
@@ -1,6 +1,6 @@
 import type { About } from './types';
 
-const AboutProjectText = `The main goal of creating Game Shop was to improve our technical skills and gain valuable experience in team development. We aimed not only to deepen our knowledge of modern web technologies but also to learn how to collaborate effectively, distribute tasks, and deliver a high-quality product within set deadlines.
+const AboutProjectText = `The main goal of creating «Game Shop» was to improve our technical skills and gain valuable experience in team development. We aimed not only to deepen our knowledge of modern web technologies but also to learn how to collaborate effectively, distribute tasks, and deliver a high-quality product within set deadlines.
 
 The project is built on Feature-Sliced Design architecture using React and TypeScript. For creating a modern and responsive interface, we chose Material UI, and the project is built using Vite.
 
@@ -10,7 +10,7 @@ const AboutTeamText = `We are a team of like-minded individuals, each of whom ma
 
 Game Shop is not only the result of our work but also a reflection of our commitment to professional growth, love for technology, and desire to create useful products for people.
 
-We hope you enjoy our project. We put a lot of effort into it ^_^`;
+We hope you enjoy our project. We put a lot of effort into it ☺️`;
 
 const AboutProject: About = {
   title: 'About the Project',
