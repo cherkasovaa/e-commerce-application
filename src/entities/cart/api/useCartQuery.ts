5b6cbdee415dae69794b3a6594b1a283264a5e09
cherkasovaa <alina.ch1202@gmail.com>
2025-06-16 16:24:23 +0300

    feat: add cart merging logic after user authentication

--- src/entities/cart/api/useCartQuery.ts
+++ src/entities/cart/api/useCartQuery.ts
@@ -1,28 +1,38 @@
-import { useQuery } from '@tanstack/react-query';
 import { getApiRoot } from '@/shared/api/commerceTools';
-import type { Cart } from '@commercetools/platform-sdk';
-import { isHttpError } from '../model';
 import { localStorageService } from '@/shared/lib/localStorage/localStorageService';
+import type { Cart } from '@commercetools/platform-sdk';
+import { useQuery } from '@tanstack/react-query';
+import { isHttpError } from '../model';
 
-export const useCartQuery = () =>
-  useQuery<Cart | null>({
+export const useCartQuery = () => {
+  const isAuthenticated = localStorageService.getAuthStatus();
+
+  return useQuery<Cart | null>({
     queryKey: ['cart'],
     retry: false,
     queryFn: async () => {
       try {
-        const anonymousId = localStorageService.getAnonymousID();
+        if (isAuthenticated) {
+          const res = await getApiRoot().me().activeCart().get().execute();
+          return res.body;
+        } else {
+          const anonymousId = localStorageService.getAnonymousID();
 
-        const res = await getApiRoot()
-          .me()
-          .activeCart()
-          .get({
-            queryArgs: anonymousId ? { anonymousId } : {},
-          })
-          .execute();
-        return res.body;
+          if (!anonymousId) return null;
+
+          const res = await getApiRoot()
+            .me()
+            .activeCart()
+            .get({
+              queryArgs: { anonymousId },
+            })
+            .execute();
+          return res.body;
+        }
       } catch (error) {
         if (isHttpError(error) && error.statusCode === 404) return null;
         throw error;
       }
     },
   });
+};
