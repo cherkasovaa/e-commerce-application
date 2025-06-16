6c13185b91f446bff23accbb87a6c1506d399db8
cherkasovaa <alina.ch1202@gmail.com>
2025-06-16 14:50:52 +0300

    fix: remove loader when changing product quantity
    Loader was causing visual flickering of product card
    when updating purchase quantity

--- src/features/cart/ui/CartItem.tsx
+++ src/features/cart/ui/CartItem.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
+import { useRemoveCartItem, useUpdateItemQuantity } from '@/entities/cart';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import RemoveIcon from '@mui/icons-material/Remove';
 import {
-  Paper,
-  Stack,
   Avatar,
   Box,
-  Typography,
   IconButton,
+  Paper,
+  Stack,
+  Typography,
 } from '@mui/material';
+import React from 'react';
 import type { CartItemProps } from '../model';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { useRemoveCartItem } from '@/entities/cart';
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import { useUpdateItemQuantity } from '@/entities/cart';
-import { Loader } from '@/shared/ui';
 
 export const CartItem: React.FC<CartItemProps> = ({ item }) => {
   const name = item.name?.['en-US'];
@@ -22,11 +20,9 @@ export const CartItem: React.FC<CartItemProps> = ({ item }) => {
   const totalPrice = (item.totalPrice.centAmount / 100).toFixed(2);
   const currency = item.price.value.currencyCode;
   const imageUrl = item.variant.images?.[0]?.url;
-  const { mutate: remove, isPending: isRemoving } = useRemoveCartItem();
-  const { mutate: updateQuantity, isPending: isUpdating } =
-    useUpdateItemQuantity();
+  const { mutate: remove } = useRemoveCartItem();
+  const { mutate: updateQuantity } = useUpdateItemQuantity();
 
-  const isLoading = isRemoving || isUpdating;
   const handleRemove = () => {
     remove(item.id);
   };
@@ -37,44 +33,40 @@ export const CartItem: React.FC<CartItemProps> = ({ item }) => {
 
   return (
     <Paper variant="outlined" sx={{ p: 2, minHeight: 150 }}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <Stack direction="row" spacing={2} alignItems="center">
-          {imageUrl && (
-            <Avatar
-              variant="rounded"
-              src={imageUrl}
-              alt={name}
-              sx={{ width: 64, height: 64 }}
-            />
-          )}
-          <Box flex={1}>
-            <Typography variant="body1">{name}</Typography>{' '}
-            <Stack direction="row" spacing={1} alignItems="center">
-              <IconButton
-                onClick={() => handleChangeQuantity(quantity - 1)}
-                disabled={quantity === 1}
-              >
-                <RemoveIcon />
-              </IconButton>
-              <Typography>{quantity}</Typography>
-              <IconButton onClick={() => handleChangeQuantity(quantity + 1)}>
-                <AddIcon />
-              </IconButton>
-            </Stack>
-            <Typography variant="body2" color="text.secondary">
-              {unitPrice} {currency}
-            </Typography>
-            <Typography variant="body2" fontWeight="bold">
-              Total: {totalPrice} {currency}
-            </Typography>
-          </Box>{' '}
-          <IconButton color="error" onClick={handleRemove}>
-            <DeleteIcon />
-          </IconButton>
-        </Stack>
-      )}
+      <Stack direction="row" spacing={2} alignItems="center">
+        {imageUrl && (
+          <Avatar
+            variant="rounded"
+            src={imageUrl}
+            alt={name}
+            sx={{ width: 64, height: 64 }}
+          />
+        )}
+        <Box flex={1}>
+          <Typography variant="body1">{name}</Typography>{' '}
+          <Stack direction="row" spacing={1} alignItems="center">
+            <IconButton
+              onClick={() => handleChangeQuantity(quantity - 1)}
+              disabled={quantity === 1}
+            >
+              <RemoveIcon />
+            </IconButton>
+            <Typography>{quantity}</Typography>
+            <IconButton onClick={() => handleChangeQuantity(quantity + 1)}>
+              <AddIcon />
+            </IconButton>
+          </Stack>
+          <Typography variant="body2" color="text.secondary">
+            {unitPrice} {currency}
+          </Typography>
+          <Typography variant="body2" fontWeight="bold">
+            Total: {totalPrice} {currency}
+          </Typography>
+        </Box>{' '}
+        <IconButton color="error" onClick={handleRemove}>
+          <DeleteIcon />
+        </IconButton>
+      </Stack>
     </Paper>
   );
 };
