3dd936187f67ca6021ab19614fa40162f3b7de95
Анастасия <anastasia@MacBook-Air-Anastasia.local>
2025-06-16 13:24:57 +0300

    refactor: change hook useCartQuery

--- src/entities/cart/api/useCartQuery.ts
+++ src/entities/cart/api/useCartQuery.ts
@@ -1,9 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import { getActiveCart } from './getActiveCart';
+import { getApiRoot } from '@/shared/api/commerceTools';
 import type { Cart } from '@commercetools/platform-sdk';
+import { isHttpError } from '../model';
 
 export const useCartQuery = () =>
   useQuery<Cart | null>({
     queryKey: ['cart'],
-    queryFn: getActiveCart,
+    queryFn: async () => {
+      try {
+        const res = await getApiRoot().me().activeCart().get().execute();
+        return res.body;
+      } catch (error) {
+        if (isHttpError(error) && error.statusCode === 404) return null;
+        throw error;
+      }
+    },
   });
